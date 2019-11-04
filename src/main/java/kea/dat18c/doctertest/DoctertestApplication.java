f565dc6c57f83225a6fccb8f23962a26eda4cf4c
Jarrald <novijgod@yahoo.dk>
2019-11-04 09:47:50 +0100

    Setup

---
+++ src/main/java/kea/dat18c/doctertest/DoctertestApplication.java
@@ -0,0 +1,13 @@
+package kea.dat18c.doctertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctertestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kea/dat18c/doctertest/DoctertestApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.dat18c.doctertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
